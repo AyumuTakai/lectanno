33ba1d67d69a284910b8970814f9251234dbca2a
AyumuTakai <atakai@hackwork.jp>
2024-10-28 10:36:06 +0900

    svgとpdfのスクロール同期

--- source_preload.js
+++ source_preload.js
@@ -20,16 +20,12 @@ function addLine(svg, x1, y1, x2, y2) {
 	line.setAttribute("stroke-width", width);
 	line.setAttribute("stroke-opacity", opacity);
 	svg.appendChild(line);
+	return line;
 }
 
 window.addEventListener("load", () => {
-	// document.querySelector("h1").textContent = "test";
-
-	// const script = document.createElement("script");
-	// script.innerHTML = `
-
-	// `;
-	// document.querySelector("body").appendChild(script);
+	let isDragging = false;
+	let currentFigure = null;
 
 	const div = document.createElement("div");
 	div.style.position = "fixed";
@@ -39,15 +35,55 @@ window.addEventListener("load", () => {
 	div.style.left = 0;
 	div.style.background = "rgba(0,0,0,0.0)";
 	div.style.zIndex = 999;
+	div.style.overflow = "hidden";
 	div.addEventListener("wheel", (event) => {
 		const container = document.querySelector("#viewerContainer");
 		console.log({ event });
 		container.scrollBy(0, event.deltaY);
+		div.scrollBy(0, event.deltaY);
 	});
 	document.querySelector("body").appendChild(div);
+
 	const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
-	svg.setAttribute("width", "100%");
-	svg.setAttribute("height", "100%");
-	addLine(svg, 0, 0, 100, 100);
+
+	svg.addEventListener("mousedown", (event) => {
+		isDragging = true;
+		if (isDragging) {
+			currentFigure = addLine(
+				svg,
+				event.offsetX,
+				event.offsetY,
+				event.offsetX,
+				event.offsetY,
+			);
+		} else {
+			currentFigure = null;
+		}
+	});
+	svg.addEventListener("mouseup", (event) => {
+		if (isDragging && currentFigure) {
+			currentFigure.setAttribute("x2", event.offsetX);
+			currentFigure.setAttribute("y2", event.offsetY);
+		}
+		isDragging = false;
+	});
+	svg.addEventListener("mousemove", (event) => {
+		if (isDragging && currentFigure) {
+			currentFigure.setAttribute("x2", event.offsetX);
+			currentFigure.setAttribute("y2", event.offsetY);
+		}
+	});
+
+	const container = document.querySelector("#viewer");
+	const resizeObserver = new ResizeObserver((entries) => {
+		const w = entries[0].contentRect.width;
+		const h = entries[0].contentRect.height;
+		console.log({ w, h });
+		svg.setAttribute("width", w);
+		svg.setAttribute("height", h);
+	});
+	resizeObserver.observe(container);
+	svg.background = "blue";
+	// addLine(svg, 0, 0, 100, 100);
 	div.appendChild(svg);
 });
